f65bb8b2af38bcf53e7cfdd26ce4a54480f9524f
Rustymattok <mrArxi@gmail.com>
2020-09-30 15:22:37 +0300

    -add HashMap(compare Mid and Bad case)

--- task3/src/main/java/ru/makarov/logic/ListUniqSum.java
+++ task3/src/main/java/ru/makarov/logic/ListUniqSum.java
@@ -1,16 +1,40 @@
 package ru.makarov.logic;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class ListUniqSum {
     public static void main(String[] args) {
-        int[] a = {3,2,1,-1,5};
+        int[] a = {3, 2, 1, -1, 5, 1, 3 ,7};
+         /*
+          Bad case:
+            O value -> n*n*n
+          Mid case:
+            O value -> n*n*1
+         */
+        Hashtable<Integer,Integer> map = new Hashtable<>();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                    map.put(a[i] + a[j], i);
+            }
+        }
+        /*
+          Bad case:
+          O value -> n*n*log(n)
+          Mid case:
+          O value -> n*n*log(n)
+         */
         TreeSet<Integer> list = new TreeSet<>();
         for (int i = 0; i < a.length; i++) {
             for (int j = i+1; j < a.length; j++) {
                 list.add(a[i] + a[j]);
             }
         }
+
         System.out.println(list);
+        for (Map.Entry<Integer, Integer> value : map.entrySet()) {
+            System.out.print(value.getKey() + " ");
+        }
+
     }
 }
